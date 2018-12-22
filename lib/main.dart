05daec135f509e218e17706fc015e6712aadf587
Joemar Taganna <joemar.ct@gmail.com>
2018-12-22 20:11:56 +0800

    Added key pair generation and message signing

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,23 @@
 import 'dart:async';
+import "package:hex/hex.dart";
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:qr_reader/qr_reader.dart';
 import 'package:local_auth/local_auth.dart';
+import 'package:flutter_sodium/flutter_sodium.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:typed_data';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Paytaca',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.red,
       ),
       home: MyHomePage(title: 'Paytaca'),
@@ -32,15 +27,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -58,13 +44,11 @@ class _MyHomePageState extends State<MyHomePage> {
   List<BiometricType> _availableBiometrics;
   String _authorized = 'Not Authorized';
 
+  String publicKey = '';
+  String privateKey = '';
+
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -114,18 +98,55 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Uint8List publicKeyBytes;
+  Uint8List privateKeyBytes;
+
+  Future<Null> generateKeyPair() async {
+    final keyPair = await CryptoSign.generateKeyPair();
+
+    setState(() {
+      publicKeyBytes = keyPair.publicKey;
+      privateKeyBytes = keyPair.secretKey;
+      publicKey = HEX.encode(keyPair.publicKey);
+      privateKey = HEX.encode(keyPair.secretKey);
+    });
+  }
+
+  Future<String> signTransaction(String txnHash) async {
+    final signature = await CryptoSign.sign(txnHash, privateKeyBytes);
+    return HEX.encode(signature);
+  }
+
+  String userXID = '';
+
+  Future<Null> createUser() async {
+    var url = "http://af056012.ngrok.io/api/users/create";
+    var payload = {
+      "firstname": "Bernardo",
+      "lastname": "Carpio",
+      "birthday": "2006-01-02",
+      "email": "joemar.ct+00" + _counter.toString() + "@gmail.com",
+      "public_key": publicKey,
+      "txn_hash": "helloworld",
+      "signature": await signTransaction("helloworld")
+    };
+    final response = await http.post(
+      url,
+      body: json.encode(payload),
+      headers: {"Content-Type": "application/json"}
+    );
+    if (response.statusCode == 200) {
+      final responseJson = json.decode(response.body);
+      setState(() {
+        userXID = responseJson['xid'];
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         centerTitle: true,
       ),
@@ -161,76 +182,78 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            new QrImage(
-              data: '$_counter',
-              size: 200.0,
-            ),
-            new RaisedButton(
-              child: const Text('Scan'),
-              onPressed: () {
-                setState(() {
-                  _barcodeString = new QRCodeReader()
-                  .setTorchEnabled(true)
-                  .setHandlePermissions(true)
-                  .setExecuteAfterPermissionGranted(true)
-                  .scan();
-                });
-              },
-            ),
-            new FutureBuilder<String>(
-              future: _barcodeString,
-              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                return new Text(snapshot.data != null ? snapshot.data : '');
-            }),
-            Text('Can check biometrics: $_canCheckBiometrics\n'),
-            RaisedButton(
-              child: const Text('Check biometrics'),
-              onPressed: _checkBiometrics,
-            ),
-            Text('Available biometrics: $_availableBiometrics\n'),
-            RaisedButton(
-              child: const Text('Get available biometrics'),
-              onPressed: _getAvailableBiometrics,
-            ),
-            Text('Current State: $_authorized\n'),
-            RaisedButton(
-              child: const Text('Authenticate'),
-              onPressed: _authenticate,
+        child: new Container(
+          child: new SingleChildScrollView(
+            child: new ConstrainedBox(
+              constraints: new BoxConstraints(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                  new QrImage(
+                    data: '$_counter',
+                    size: 200.0,
+                  ),
+                  new RaisedButton(
+                    child: const Text('Scan'),
+                    onPressed: () {
+                      setState(() {
+                        _barcodeString = new QRCodeReader()
+                        .setTorchEnabled(true)
+                        .setHandlePermissions(true)
+                        .setExecuteAfterPermissionGranted(true)
+                        .scan();
+                      });
+                    },
+                  ),
+                  new FutureBuilder<String>(
+                    future: _barcodeString,
+                    builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                      return new Text(snapshot.data != null ? snapshot.data : '');
+                  }),
+                  Text('Can check biometrics: $_canCheckBiometrics\n'),
+                  RaisedButton(
+                    child: const Text('Check biometrics'),
+                    onPressed: _checkBiometrics,
+                  ),
+                  Text('Available biometrics: $_availableBiometrics\n'),
+                  RaisedButton(
+                    child: const Text('Get available biometrics'),
+                    onPressed: _getAvailableBiometrics,
+                  ),
+                  Text('Current State: $_authorized\n'),
+                  RaisedButton(
+                    child: const Text('Authenticate'),
+                    onPressed: _authenticate,
+                  ),
+                  Text('Public key: $publicKey\n'),
+                  Text('Private key: $privateKey\n'),
+                  RaisedButton(
+                    child: const Text('Generate Key Pair'),
+                    onPressed: generateKeyPair,
+                  ),
+                  Text('User XID: $userXID\n'),
+                  RaisedButton(
+                    child: const Text('Create User'),
+                    onPressed: createUser,
+                  ),
+                ],
+              ),
             )
-          ],
+            )
+          )
         ),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
