adc83219ce578b02416c37df76ff5bd5347cbc9a
Joemar Taganna <joemar.ct@gmail.com>
2019-02-04 12:22:55 +0800

    Ability to select account for sending and receiving funds

--- lib/views/receive.dart
+++ lib/views/receive.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:flutter_keychain/flutter_keychain.dart';
+import 'package:swipedetector/swipedetector.dart';
 import '../components/drawer.dart';
 import '../components/bottomNavigation.dart';
 import '../api/responses.dart';
@@ -12,6 +13,8 @@ class ReceiveComponent extends StatefulWidget {
 
 class ReceiveComponentState extends State<ReceiveComponent> {
   String path = "/receive";
+
+  int accountIndex = 0;
 
   Future<List<Account>> getAccountsFromKeychain() async {
     String accounts = await FlutterKeychain.get(key: "accounts");
@@ -39,16 +42,39 @@ class ReceiveComponentState extends State<ReceiveComponent> {
             future: getAccountsFromKeychain(),
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if (snapshot.hasData) {
-                return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(snapshot.data[0].accountName),
-                      QrImage(
-                        data: snapshot.data[0].accountId,
-                        size: 0.6 * bodyHeight,
-                      )
-                    ]);
+                return SwipeDetector(
+                    onSwipeLeft: () {
+                      setState(() {
+                        if (accountIndex < (snapshot.data.length - 1)) {
+                          accountIndex += 1;
+                        }
+                      });
+                    },
+                    onSwipeRight: () {
+                      setState(() {
+                        if (accountIndex > 0) {
+                          accountIndex -= 1;
+                        }
+                      });
+                    },
+                    swipeConfiguration: SwipeConfiguration(
+                      verticalSwipeMinVelocity: 100.0,
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            snapshot.data[accountIndex].accountName,
+                            style: new TextStyle(
+                              fontSize: 18.0,
+                            ),
+                          ),
+                          QrImage(
+                            data: snapshot.data[accountIndex].accountId,
+                            size: 0.6 * bodyHeight,
+                          )
+                        ]));
               } else {
                 return Column(
                     mainAxisAlignment: MainAxisAlignment.center,
--- lib/views/send.dart
+++ lib/views/send.dart
@@ -2,11 +2,13 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:qr_reader/qr_reader.dart';
 import 'package:flutter_keychain/flutter_keychain.dart';
+import 'package:swipedetector/swipedetector.dart';
 import '../components/bottomNavigation.dart';
 import '../components/drawer.dart';
 import '../api/endpoints.dart';
 import '../views/app.dart';
 import '../helpers.dart';
+import '../api/responses.dart';
 
 class SendComponent extends StatefulWidget {
   @override
@@ -26,14 +28,30 @@ class SendComponentState extends State<SendComponent> {
   Future<String> _barcodeString;
   String path = '/send';
 
-  Future<bool> sendFunds(String toAccount, int amount) async {
+  int accountIndex = 0;
+  List<Account> accounts = [];
+
+  Future<List<Account>> getAccountsFromKeychain() async {
+    String accounts = await FlutterKeychain.get(key: "accounts");
+    List<Account> _accounts = [];
+    for (final acct in accounts.split(',')) {
+      var acctObj = new Account();
+      acctObj.accountName = acct.split('|')[0];
+      acctObj.accountId = acct.split('|')[1];
+      _accounts.add(acctObj);
+    }
+    return _accounts;
+  }
+
+  Future<bool> sendFunds(
+      String toAccount, int amount, BuildContext context) async {
     String publicKey = await FlutterKeychain.get(key: "publicKey");
     String privateKey = await FlutterKeychain.get(key: "privateKey");
-    String accounts = await FlutterKeychain.get(key: "accounts");
+    // String accounts = await FlutterKeychain.get(key: "accounts");
     final String txnhash = _randomString(20);
     String signature = await signTransaction(txnhash, privateKey);
     var payload = {
-      'from_account': accounts.split(',')[0].split('|')[1],
+      'from_account': accounts[accountIndex].accountId,
       'to_account': toAccount,
       'asset': 'BABE6CFE-A5C7-445C-9225-B072B98EBEA6',
       'amount': amount,
@@ -42,6 +60,20 @@ class SendComponentState extends State<SendComponent> {
       "signature": signature
     };
     var response = await transferAsset(payload);
+    showDialog(
+        context: context,
+        builder: (_) => new AlertDialog(
+              title: new Text("Funds Sent"),
+              content: new Text("PHP $sendAmount was sent successfully!"),
+              actions: <Widget>[
+                new FlatButton(
+                  child: new Text("OK"),
+                  onPressed: () {
+                    Application.router.navigateTo(context, "/home");
+                  },
+                ),
+              ],
+            ));
     return response.success;
   }
 
@@ -62,18 +94,60 @@ class SendComponentState extends State<SendComponent> {
               new SizedBox(
                 height: 30.0,
               ),
-              new RaisedButton(
-                child: const Text('Scan QR Code'),
-                onPressed: () {
-                  setState(() {
-                    _barcodeString = new QRCodeReader()
-                        .setTorchEnabled(true)
-                        .setHandlePermissions(true)
-                        .setExecuteAfterPermissionGranted(true)
-                        .scan();
-                  });
-                },
-              ),
+              Center(
+                  child: FutureBuilder(
+                      future: getAccountsFromKeychain(),
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        if (snapshot.hasData) {
+                          accounts = snapshot.data;
+                          String sourceAccount =
+                              snapshot.data[accountIndex].accountName;
+                          return SwipeDetector(
+                              onSwipeLeft: () {
+                                setState(() {
+                                  if (accountIndex <
+                                      (snapshot.data.length - 1)) {
+                                    accountIndex += 1;
+                                  }
+                                });
+                              },
+                              onSwipeRight: () {
+                                setState(() {
+                                  if (accountIndex > 0) {
+                                    accountIndex -= 1;
+                                  }
+                                });
+                              },
+                              swipeConfiguration: SwipeConfiguration(
+                                verticalSwipeMinVelocity: 100.0,
+                              ),
+                              child: new Container(
+                                  padding: const EdgeInsets.only(
+                                      top: 50.0, bottom: 50.00),
+                                  child: Text(
+                                    'Send from $sourceAccount account',
+                                    style: new TextStyle(
+                                      fontSize: 18.0,
+                                    ),
+                                  )));
+                        } else {
+                          return Text('Fetching accounts...');
+                        }
+                      })),
+              new Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: new RaisedButton(
+                    child: const Text('Scan QR Code'),
+                    onPressed: () {
+                      setState(() {
+                        _barcodeString = new QRCodeReader()
+                            .setTorchEnabled(true)
+                            .setHandlePermissions(true)
+                            .setExecuteAfterPermissionGranted(true)
+                            .scan();
+                      });
+                    },
+                  )),
               new FutureBuilder<String>(
                   future: _barcodeString,
                   builder:
@@ -94,27 +168,10 @@ class SendComponentState extends State<SendComponent> {
                             visible: snapshot.data != null),
                         Visibility(
                             child: new RaisedButton(
-                              child: const Text("Send"),
-                              onPressed: () {
-                                sendFunds(snapshot.data, sendAmount);
-                                showDialog(
-                                    context: context,
-                                    builder: (_) => new AlertDialog(
-                                          title: new Text("Funds Sent"),
-                                          content: new Text(
-                                              "PHP $sendAmount was sent successfully!"),
-                                          actions: <Widget>[
-                                            new FlatButton(
-                                              child: new Text("OK"),
-                                              onPressed: () {
-                                                Application.router.navigateTo(
-                                                    context, "/home");
-                                              },
-                                            ),
-                                          ],
-                                        ));
-                              },
-                            ),
+                                child: const Text("Send"),
+                                onPressed: () {
+                                  sendFunds(snapshot.data, sendAmount, context);
+                                }),
                             visible: snapshot.data != null)
                       ],
                     );
