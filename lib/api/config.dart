eb104465b47b43c10381f52599617619fa6bf181
Joemar Taganna <joemar.ct@gmail.com>
2019-02-14 18:33:47 +0800

    Updated PHP asset ID

--- lib/api/config.dart
+++ lib/api/config.dart
@@ -1,2 +1,2 @@
 final String baseUrl = 'https://lantaka-dev.paytaca.com';
-final String phpAssetId = 'EEF7158A-EB1D-4B7E-95FC-FEE2AF89BFC4';
+final String phpAssetId = '87680574-8EC2-440A-A381-C33EE987FCE4';
