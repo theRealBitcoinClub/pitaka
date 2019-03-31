25c25e6892652952ca19f57ec605c8b7141eeeff
Reamon Sumapig <29reamonsumapig@gmail.com>
2019-03-31 16:49:34 +0800

    Set Business Page Improvement -Modify/enhance dropdown button. -Added alert dialog after successful linking of business and account. Ref Issue #13

--- lib/views/setBusinessAccount.dart
+++ lib/views/setBusinessAccount.dart
@@ -3,13 +3,10 @@
 import 'package:flutter/material.dart';
 import '../api/endpoints.dart';
 import '../views/app.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
 
-class BusinessAccount {
-  String name;
-  String type;
-  String tin;
-  String address;
+class FormAccount {
+  String paytacaAccount;
+  String businessAccount;
 }
 
 class SetBusinessAccountComponent extends StatefulWidget {
@@ -19,7 +16,40 @@ class SetBusinessAccountComponent extends StatefulWidget {
 
 class SetBusinessAccountComponentState extends State<SetBusinessAccountComponent> {
   final _formKey = GlobalKey<FormState>();
-  BusinessAccount businessInfo = new BusinessAccount();
+  FormAccount formInfo = new FormAccount();
+  List<String> _businessType = <String>['Business1','Business2', 'Business3'];
+  String _selectedType = 'Business1';
+
+  List<String> _paytacaAccounts = <String>['Account1','Account2', 'Account3'];
+  String _selectedPaytacaAccount = 'Account1';
+
+  Future<void> _neverSatisfied() async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Success'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('Your account and bussiness are now connected.')
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Got It!'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              Application.router.navigateTo(context, "/businesstools");
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
 
   String validateCompanyName(String value) {
     if (value.length == 0) {
@@ -60,12 +90,15 @@ class SetBusinessAccountComponentState extends State<SetBusinessAccountComponent
 
   void _submitForm(BuildContext context) async {
     var valid = _formKey.currentState.validate();
+    print('a');
     if (valid) {
+      print('b');
+      _neverSatisfied();
       // Close the on-screen keyboard by removing focus from the form's inputs
       FocusScope.of(context).requestFocus(new FocusNode());
       // Save the form
       _formKey.currentState.save();
-      var response = await registerBusiness(businessInfo);
+      var response = await registerBusiness(formInfo);
       print(response);
       //await FlutterKeychain.put(key: "defaultAccount", value: response.id);
       Application.router.navigateTo(context, "/home");
@@ -81,26 +114,65 @@ class SetBusinessAccountComponentState extends State<SetBusinessAccountComponent
           new SizedBox(
             height: 30.0,
           ),
-          new DropdownButton<String>(
-            items: <String>['A', 'B', 'C', 'D'].map((String value) {
-              return new DropdownMenuItem<String>(
-                value: value,
-                child: new Text(value),
+          new FormField(
+            builder: (FormFieldState state) {
+              return InputDecorator(
+                decoration: InputDecoration(
+                  icon: const Icon(Icons.account_box),
+                  labelText: 'Paytaca',
+                ),
+                isEmpty: _selectedPaytacaAccount == '',
+                child: new DropdownButtonHideUnderline(
+                  child: new DropdownButton(
+                    value: _selectedPaytacaAccount,
+                    isDense: true,
+                    onChanged: (String newValue) {
+                      setState(() {
+                        _selectedPaytacaAccount = newValue;
+                        formInfo.paytacaAccount = newValue;
+                        state.didChange(newValue);
+                      });
+                    },
+                    items: _paytacaAccounts.map((String value) {
+                      return new DropdownMenuItem(
+                        value: value,
+                        child: new Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
               );
-            }).toList(),
-            onChanged: (_) {},
+            },
           ),
-          new SizedBox(
-            height: 30.0,
-          ),
-          new DropdownButton<String>(
-            items: <String>['A', 'B', 'C', 'D'].map((String value) {
-              return new DropdownMenuItem<String>(
-                value: value,
-                child: new Text(value),
+          new FormField(
+            builder: (FormFieldState state) {
+              return InputDecorator(
+                decoration: InputDecoration(
+                  icon: const Icon(Icons.account_balance_wallet),
+                  labelText: 'Business',
+                ),
+                isEmpty: _selectedType == '',
+                child: new DropdownButtonHideUnderline(
+                  child: new DropdownButton(
+                    value: _selectedType,
+                    isDense: true,
+                    onChanged: (String newValue) {
+                      setState(() {
+                        _selectedType = newValue;
+                        formInfo.businessAccount = newValue;
+                        state.didChange(newValue);
+                      });
+                    },
+                    items: _businessType.map((String value) {
+                      return new DropdownMenuItem(
+                        value: value,
+                        child: new Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
               );
-            }).toList(),
-            onChanged: (_) {},
+            },
           ),
           new SizedBox(
             height: 30.0,
