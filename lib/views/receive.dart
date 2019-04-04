3f7146afd716d6e4eeace136bdfae577d31fc9ce
Reamon Sumapig <29reamonsumapig@gmail.com>
2019-04-04 20:11:34 +0800

    on going changes Ref Issue #15

--- lib/views/receive.dart
+++ lib/views/receive.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:flutter_keychain/flutter_keychain.dart';
 import 'package:swipedetector/swipedetector.dart';
+import '../views/app.dart';
+import '../api/config.dart';
+import 'package:crypto/crypto.dart';
 import 'dart:async';
 import '../components/drawer.dart';
 import '../components/bottomNavigation.dart';
 import '../api/responses.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter_string_encryption/flutter_string_encryption.dart';
+
 
 class ReceiveComponent extends StatefulWidget {
   @override
@@ -14,9 +20,9 @@ class ReceiveComponent extends StatefulWidget {
 
 class ReceiveComponentState extends State<ReceiveComponent> {
   String path = "/receive";
-
+  String _qrCodeString = "";
   int accountIndex = 0;
-
+  
   Future<List<Account>> getAccountsFromKeychain() async {
     String accounts = await FlutterKeychain.get(key: "accounts");
     List<Account> _accounts = [];
@@ -27,6 +33,52 @@ class ReceiveComponentState extends State<ReceiveComponent> {
       _accounts.add(acctObj);
     }
     return _accounts;
+  }
+
+  void scanQrcode() async {
+    String qrcode = await FlutterBarcodeScanner.scanBarcode("#ff6666");
+    setState(() => _qrCodeString = qrcode);
+    final cryptor = new PlatformStringCryptor();
+    final String salt = await cryptor.generateSalt();
+    final String securityKey = await cryptor.generateKeyFromPassword(hashCode, salt);
+    try {
+      final String decrypted = await cryptor.decrypt(_qrCodeString, securityKey);
+      print(decrypted); // - A string to encrypt.
+    } on MacMismatchException {
+      print('Error po!');
+      // unable to decrypt (wrong key or forged data)
+    }
+    // _qrCodeString
+    _successDialog();
+  }
+
+
+  Future<void> _successDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Success'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Business account was successufully added.')
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Got It!'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Application.router.navigateTo(context, "/businesstools");
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -75,6 +127,12 @@ class ReceiveComponentState extends State<ReceiveComponent> {
                         QrImage(
                           data: snapshot.data[accountIndex].accountId,
                           size: 0.6 * bodyHeight,
+                        ),
+                        new RaisedButton(
+                          child: const Text('Scan Proof'),
+                          onPressed: () {
+                            scanQrcode();
+                          },
                         )
                       ]
                     )
