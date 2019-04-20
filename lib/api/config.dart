7e8efb7fc75fb5b3586e63df39b68ed75b96ef0c
Reamon Sumapig <29reamonsumapig@gmail.com>
2019-04-20 17:59:43 +0800

    Fixed issue by switching stickyAuth = true

--- lib/api/config.dart
+++ lib/api/config.dart
@@ -1,3 +1,2 @@
-// final String baseUrl = 'https://lantaka-dev.paytaca.com';
-final String baseUrl = 'https://513a8107.ngrok.io';
+final String baseUrl = 'https://lantaka-dev.paytaca.com';
 final String phpAssetId = '87680574-8EC2-440A-A381-C33EE987FCE4';
