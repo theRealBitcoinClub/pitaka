d1efbffa8875cc609aa4ed653101bd03e874bfa0
Reamon Sumapig <29reamonsumapig@gmail.com>
2019-04-22 20:08:36 +0800

    initial codes for database setup

--- lib/views/app.dart
+++ lib/views/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
+import 'package:path/path.dart';
 import '../router/routes.dart';
+import 'package:sqflite/sqflite.dart';
 
 class AppComponent extends StatefulWidget {
   @override
@@ -22,8 +24,34 @@ class AppComponentState extends State<AppComponent> {
     Application.router = router;
   }
 
+  createDatabase() async {
+    String databasesPath = await getDatabasesPath();
+    String dbPath = join(databasesPath, 'my.db');
+
+    var database = await openDatabase(dbPath, version: 1, onCreate: populateDb);
+    return database;
+  }
+
+  void populateDb(Database database, int version) async {
+    await database.execute("CREATE TABLE Balance ("
+      "id INTEGER PRIMARY KEY,"
+      "account TEXT,"
+      "balance TEXT,"
+      "timestamp TEXT,"
+      "signature TEXT,"
+      "datecreated TEXT"
+      ")");
+
+    await database.execute("CREATE TABLE OfflineTransaction ("
+      "id INTEGER PRIMARY KEY,"
+      "timestamp TEXT,"
+      "signature TEXT,"
+      ")");
+  }
+
   @override
   Widget build(BuildContext context) {
+    createDatabase();
     final app = new MaterialApp(
       title: 'Paytaca',
       debugShowCheckedModeBanner: debugMode,
@@ -34,4 +62,6 @@ class AppComponentState extends State<AppComponent> {
     );
     return app;
   }
+
+  getDatabasesPath() {}
 }
