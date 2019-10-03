6e2e23fceb667e5828f6ab80ac7b0d84137da9ff
annagat <jtaganna@gmail.com>
2019-10-03 10:43:05 +0800

    Added code to prompt user to enter pincode after user registration

--- lib/views/addPinCode.dart
+++ lib/views/addPinCode.dart
@@ -1,46 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:pinput/pin_put/pin_put.dart';
+import '../utils/globals.dart' as globals;
+import '../views/app.dart';
 
-class AddPinCodeComponent extends StatefulWidget {
-  @override
-  State createState() => new AddPinCodeComponentState();
-}
+void main() => runApp(AddPinCodeComponent());
 
-class AddPinCodeComponentState extends State<AddPinCodeComponent> {
+class AddPinCodeComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-          primaryColor: Colors.green,
-          hintColor: Colors.green,
-        ),
-        home: Scaffold(
-          body: Builder(
-            builder: (context) => Padding(
-                padding: const EdgeInsets.all(40.0),
-                child: Center(
-                  child: PinPut(
-                    fieldsCount: 4,
-                    onSubmit: (String pin) => _showSnackBar(pin, context),
-                  ),
-                ),
-              ),
-        )));
+      title: 'Retrieve Text Input',
+      home: MyCustomForm(),
+    );
+  }
+}
+
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+// Define a corresponding State class.
+// This class holds the data related to the Form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+
+  String pincodeKey;
+  String pincode;
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
   }
 
-  void _showSnackBar(String pin, BuildContext context) {
-    final snackBar = SnackBar(
-      duration: Duration(seconds: 5),
-      content: Container(
-          height: 80.0,
-          child: Center(
-            child: Text(
-              'Pin Submitted. Value: $pin',
-              style: TextStyle(fontSize: 25.0),
-            ),
-          )),
-      backgroundColor: Colors.greenAccent,
-    );
-    Scaffold.of(context).showSnackBar(snackBar);
+  _storePincode() {
+    setState(() async{
+      pincode = myController.text;
+      await globals.storage.write(key: "pincodeKey", value: pincode);
+      // For debug, check if pin code was save
+      final read = await globals.storage.read(key: "pincodeKey");
+      print("The pincode: $read was succesfully save.");
+      Application.router.navigateTo(context, "/account");
+    });
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Retrieve Text Input'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: myController,
+                  autocorrect: true,
+                  decoration: InputDecoration(hintText: 'Enter pincode'),
+                ),
+              ),
+              RaisedButton(
+                onPressed: _storePincode,
+                child: Text("Submit"),
+              )
+            ],
+          ),
+        ));
+  }
+
+  // void initState() {
+  //   super.initState();
+  //   print("Saving pincode...");
+  //   WidgetsBinding.instance.addPostFrameCallback((_) => storePincode());
+  // }
+
 }
