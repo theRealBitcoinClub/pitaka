93c13e9ff3d0c3b7aaa28ae90dc70f3a3e0a5dde
annagat <jtaganna@gmail.com>
2020-02-11 15:07:05 +0800

    Added form for contact list

--- lib/views/contactList.dart
+++ lib/views/contactList.dart
@@ -8,6 +8,14 @@ import '../utils/globals.dart' as globals;
 import '../utils/globals.dart';
 
 
+class Contact {
+  String firstName;
+  String lastName;
+  String emailAddress;
+  DateTime birthDate;
+  String imei;
+}
+
 class ContactListComponent extends StatefulWidget {
   @override
   ContactListComponentState createState() => new ContactListComponentState();
@@ -106,7 +114,7 @@ class ContactListComponentState extends State<ContactListComponent> {
             );
           }
           else {
-            return new Stack(children: _buildForm(context));
+            return new Stack(children: _buildContactListForm(context));
           }
         }),
         floatingActionButton: FloatingActionButton(
@@ -131,23 +139,209 @@ class ContactListComponentState extends State<ContactListComponent> {
     );
   }
 
-  List<Widget> _buildForm(BuildContext context) {
+  // List<Widget> _buildForm(BuildContext context) {
+  //   Form form = new Form(
+  //     key: _formKey,
+  //     child: new ListView(
+  //       padding: const EdgeInsets.symmetric(horizontal: 16.0),
+  //       children: <Widget>[
+  //         new SizedBox(
+  //           height: 20.0,
+  //         ),
+  //         new SizedBox(
+  //           height: 20.0,
+  //         ),
+  //       ],
+  //     )
+  //   );
+  //   var ws = new List<Widget>();
+  //   ws.add(form);
+  //   return ws;
+  // }
+
+  User newUser = new User();
+
+  String validateName(String value) {
+    if (value.length < 2)
+      return 'Name must be at least 2 characters';
+    else
+      return null;
+  }
+
+  String validateEmail(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return 'Enter Valid Email';
+    else
+      return null;
+  }
+
+ void _validateInputs(BuildContext context) async {
+    if (_formKey.currentState.validate()) {
+      // Close the on-screen keyboard by removing focus from the form's inputs
+      FocusScope.of(context).requestFocus(new FocusNode());
+
+      if (_termsChecked) {
+        // If all data are correct then save data to out variables
+        _formKey.currentState.save();
+        await generateKeyPair(context);
+
+        if (authenticated == true) {
+          setState(() {
+            _submitting = true;
+          });
+          
+          // Get the mobile number from previous route parameter
+          mobileNumber = "${widget.mobileNumber}";
+
+          var userPayload = {
+            "firstname": newUser.firstName,
+            "lastname": newUser.lastName,
+            "birthday": "2006-01-02",
+            "email": newUser.emailAddress,
+            "mobile_number": mobileNumber,
+          };
+          String txnHash = generateTransactionHash(userPayload);
+          print("The value of txnHash is: $txnHash");
+          String signature = await signTransaction(txnHash, privateKey);
+
+          userPayload["public_key"] = publicKey;
+          userPayload["txn_hash"] = txnHash;
+          userPayload["signature"] = signature;
+          var user = await createUser(userPayload);
+          
+          // Catch duplicate email address in the error
+          if (user.error == "duplicate_email") {
+            showAlertDialog();
+          }
+
+          // Catch app version compatibility
+          if (user.error == "outdated_app_version") {
+            showOutdatedAppVersionDialog(context);
+          }
+          
+          await globals.storage.write(key: "userId", value: user.id);
+          // Login
+          String loginSignature =
+            await signTransaction("hello world", privateKey);
+          var loginPayload = {
+            "public_key": publicKey,
+            "session_key": "hello world",
+            "signature": loginSignature,
+          };
+          await loginUser(loginPayload);
+
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          await prefs.setBool('installed', true);
+          Application.router.navigateTo(context, "/addpincode");
+          databaseHelper.initializeDatabase();
+
+        }
+      } else {
+        _showSnackBar("Please agree to our Terms and Conditions");
+      }
+    } else {
+      _showSnackBar("Please correct errors in the form");
+    }
+  }
+
+  void _showSnackBar(String message) {
+    final snackBar =
+        new SnackBar(content: new Text(message), backgroundColor: Colors.red);
+    Scaffold.of(_scaffoldContext).showSnackBar(snackBar);
+  }
+
+  List<Widget> _buildContactListForm(BuildContext context) {
     Form form = new Form(
-      key: _formKey,
-      child: new ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        children: <Widget>[
-          new SizedBox(
-            height: 20.0,
-          ),
-          new SizedBox(
-            height: 20.0,
-          ),
-        ],
-      )
-    );
+        key: _formKey,
+        autovalidate: false,
+        child: new ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            children: <Widget>[
+              new SizedBox(
+                height: 30.0,
+              ),
+              new Center(
+                  child: new Text("Sign up to create your wallet",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ))),
+              new SizedBox(
+                height: 10.0,
+              ),
+              new TextFormField(
+                keyboardType: TextInputType.text,
+                validator: validateName,
+                onSaved: (value) {
+                  newUser.firstName = value;
+                },
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.person_outline),
+                  hintText: 'Enter your first name',
+                  labelText: 'First Name',
+                ),
+              ),
+              new TextFormField(
+                keyboardType: TextInputType.text,
+                validator: validateName,
+                onSaved: (value) {
+                  newUser.lastName = value;
+                },
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.person),
+                  hintText: 'Enter your last name',
+                  labelText: 'Last Name',
+                ),
+              ),
+              new TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                validator: validateEmail,
+                onSaved: (value) {
+                  newUser.emailAddress = value;
+                },
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.email),
+                  hintText: 'Enter your email address',
+                  labelText: 'Email address',
+                ),
+              ),
+              new SizedBox(
+                height: 20.0,
+              ),
+              new SizedBox(
+                height: 15.0,
+              ),
+              new RaisedButton(
+                onPressed: () {
+                  _validateInputs(context);
+                },
+                child: new Text('Submit'),
+              )
+            ]));
+
     var ws = new List<Widget>();
     ws.add(form);
+
+    if (_submitting) {
+      var modal = new Stack(
+        children: [
+          new Opacity(
+            opacity: 0.8,
+            child: const ModalBarrier(dismissible: false, color: Colors.grey),
+          ),
+          new Center(
+            child: new CircularProgressIndicator(),
+          ),
+        ],
+      );
+      ws.add(modal);
+    }
+
     return ws;
   }
+
 }
+
+
