39c57f035802dd121ff8d753c736f5855e914e3e
annagat <jtaganna@gmail.com>
2020-05-28 15:48:35 +0800

    Changed dialog to simple notification during when app is running in foreground on push notification

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,6 +3,7 @@ import 'receive.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_udid/flutter_udid.dart';
+import 'package:overlay_support/overlay_support.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
@@ -60,15 +61,18 @@ class HomeComponentState extends State<HomeComponent> {
     _firebaseMessaging.configure(
       onMessage: (Map<String, dynamic> message) async {
         print("onMessage: $message");
-        showPushNotificationDialog(context, message);
+        showSimpleNotification(
+          Text(message['notification']['body']),
+          background: Colors.red[600],
+        );
       },
       onLaunch: (Map<String, dynamic> message) async {
         print("onLaunch: $message");
-        showPushNotificationDialog(context, message);
+        //showPushNotificationDialog(context, message);
       },
       onResume: (Map<String, dynamic> message) async {
         print("onResume: $message");
-        showPushNotificationDialog(context, message);
+        //showPushNotificationDialog(context, message);
       },
     );
   }
