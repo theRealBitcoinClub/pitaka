9b521a150db60d203b0e4937087fb1537da42ec5
annagat <jtaganna@gmail.com>
2020-05-29 16:23:47 +0800

    Updated addAccount page and added widgets for business account creation

--- lib/views/addAccount.dart
+++ lib/views/addAccount.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/material.dart';
-import '../api/endpoints.dart';
-import '../views/app.dart';
-import '../utils/helpers.dart';
-import '../components/drawer.dart';
-import '../utils/globals.dart' as globals;
-import '../utils/globals.dart';
 import 'dart:async';
+import 'package:flutter/material.dart';
+import '../views/app.dart';
+import '../api/endpoints.dart';
+import '../components/drawer.dart';
+import '../utils/helpers.dart';
+import '../utils/globals.dart';
 import '../utils/dialogs.dart';
+import '../utils/globals.dart' as globals;
 
 
 class AddAccount {
@@ -15,18 +15,18 @@ class AddAccount {
 
 class AddAccountComponent extends StatefulWidget {
   @override
-  AddAccountComponentState createState() => new AddAccountComponentState();
+  AddAccountComponentState createState() => AddAccountComponentState();
 }
 
 class AddAccountComponentState extends State<AddAccountComponent> {
-
-  final _formKey = GlobalKey<FormState>();
-  bool _submitting = false;
+  StreamSubscription _connectionChangeStream;
   AddAccount newAccount = new AddAccount();
+  final _formKey = GlobalKey<FormState>();
+  String _accountType;
   bool _autoValidate = false;
   bool online = globals.online;
-  StreamSubscription _connectionChangeStream;
   bool isOffline = false;
+  bool _submitting = false;
 
 
   String validateName(String value) {
@@ -39,7 +39,7 @@ class AddAccountComponentState extends State<AddAccountComponent> {
   void _validateInputs(BuildContext context) async {
     if (_formKey.currentState.validate()) {
       // Close the on-screen keyboard by removing focus from the form's inputs
-      FocusScope.of(context).requestFocus(new FocusNode());
+      FocusScope.of(context).requestFocus(FocusNode());
       // Save the form
       _formKey.currentState.save();
       // Send request to create the account
@@ -76,50 +76,149 @@ class AddAccountComponentState extends State<AddAccountComponent> {
 
   List<Widget> _buildAccountForm(BuildContext context) {
     if (globals.online) {
-      Form form = new Form(
+      Form form = Form(
         key: _formKey,
         autovalidate: _autoValidate,
-        child: new ListView(
+        child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           children: <Widget>[
-            new SizedBox(
-              height: 30.0,
-            ),
-            new TextFormField(
-              keyboardType: TextInputType.text,
-              validator: validateName,
-              onSaved: (value) {
-                newAccount.name = value;
-              },
-              decoration: const InputDecoration(
-                icon: const Icon(Icons.person_outline),
-                hintText: 'Enter account name',
-                labelText: 'Account Name',
+            Padding(
+              padding: EdgeInsets.only(top: 30.0, left: 8.0, right: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Account Type",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  SizedBox(height: 5.0,),
+                  DropdownButton(
+                    hint: Text('Select Account Type'),
+                    iconEnabledColor: Colors.red,
+                    value: _accountType,
+                    isExpanded: true,
+                    isDense: true,
+                    iconSize: 30.0,
+                    items: [
+                      "Personal", 
+                      "Business", 
+                    ].map(
+                      (val) {
+                        return DropdownMenuItem<String>(
+                          value: val,
+                          child: Text(val),
+                        );
+                      },
+                    ).toList(),
+                    onChanged: (val) {
+                      setState(
+                        () {
+                          _accountType = val;
+                        },
+                      );
+                    },
+                  ),
+                ]
               ),
             ),
-            new SizedBox(
-              height: 30.0,
-            ),
-            new RaisedButton(
-              onPressed: () {
-                _validateInputs(context);
-              },
-              child: new Text('Create'),
-            )
+            SizedBox(height: 20.0,),
+            _accountType == "Personal" ?
+              Padding(
+                padding: EdgeInsets.only(left: 8.0, right: 8.0,),
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      validator: validateName,
+                      onSaved: (value) {
+                        newAccount.name = value;
+                      },
+                      decoration: const InputDecoration(
+                        icon: const Icon(Icons.person_outline, color: Colors.red,),
+                        hintText: 'Enter account name',
+                        labelText: 'Personal Account Name',
+                      ),
+                    ),
+                    SizedBox(height: 30.0,),
+                    SizedBox(
+                      width: double.infinity,
+                      child: RaisedButton(
+                        color: Colors.red,
+                        splashColor: Colors.red[100],
+                        textColor: Colors.white,
+                        onPressed: () {
+                          _validateInputs(context);
+                        },
+                        child: Text('Create'),
+                      )
+                    )
+                  ],
+                )
+              )
+            : _accountType == "Business" ?
+              Padding(
+                padding: EdgeInsets.only(left: 8.0, right: 8.0,),
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      validator: validateName,
+                      onSaved: (value) {
+                        newAccount.name = value;
+                      },
+                      decoration: const InputDecoration(
+                        icon: const Icon(Icons.attach_money, color: Colors.red,),
+                        hintText: 'Enter account name',
+                        labelText: 'Business Account Name',
+                      ),
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      validator: validateName,
+                      onSaved: (value) {
+                        newAccount.name = value;
+                      },
+                      decoration: const InputDecoration(
+                        icon: const Icon(Icons.insert_link, color: Colors.red,),
+                        hintText: 'Enter callback URL',
+                        labelText: 'Callback URL',
+                      ),
+                    ),
+                    SizedBox(height: 30.0,),
+                    SizedBox(
+                      width: double.infinity,
+                      child: RaisedButton(
+                        color: Colors.red,
+                        splashColor: Colors.red[100],
+                        textColor: Colors.white,
+                        onPressed: () {
+                          _validateInputs(context);
+                        },
+                        child: Text('Create'),
+                      )
+                    )
+                  ],
+                )
+              )
+            :
+              Container()
           ],
         )
       );
-      var ws = new List<Widget>();
+      var ws = List<Widget>();
       ws.add(form);
       if (_submitting) {
-        var modal = new Stack(
+        var modal = Stack(
           children: [
-            new Opacity(
+            Opacity(
               opacity: 0.8,
               child: const ModalBarrier(dismissible: false, color: Colors.grey),
             ),
-            new Center(
-              child: new CircularProgressIndicator(),
+            Center(
+              child: CircularProgressIndicator(),
             ),
           ],
         );
@@ -128,10 +227,10 @@ class AddAccountComponentState extends State<AddAccountComponent> {
       return ws;
     } else {
       return <Widget> [
-        new Center(
-          child: new Padding(
+        Center(
+          child: Padding(
               padding: EdgeInsets.all(8.0),
-              child:new Container(
+              child: Container(
               child: Text(
                 "This is not available in offline mode.",
                 style: TextStyle(fontSize: 18.0)
@@ -149,16 +248,6 @@ class AddAccountComponentState extends State<AddAccountComponent> {
     super.initState();
     ConnectionStatusSingleton connectionStatus = ConnectionStatusSingleton.getInstance();
     _connectionChangeStream = connectionStatus.connectionChange.listen(connectionChanged);
-  /*  globals.checkConnection().then((status){
-      setState(() {
-        if (status == false) {
-          online = false;  
-          globals.online = online;
-        } else {
-          globals.online = online;
-        }
-      });
-    });*/
   }
 
 
@@ -180,34 +269,22 @@ class AddAccountComponentState extends State<AddAccountComponent> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Create Account'),
-          actions: [
-            Padding(
-              padding: EdgeInsets.only(right: 20.0),
-              child: GestureDetector(
-                child: online ? new Icon(Icons.wifi): new Icon(Icons.signal_wifi_off),
-            /*    onTap: (){
-                  globals.checkConnection().then((status){
-                    setState(() {
-                      if (status == true) {
-                        online = !online;  
-                        globals.online = online;  
-                      } else {
-                        online = false;  
-                        globals.online = online;
-                      }
-                    });
-                  });
-                }*/
-              )
+      appBar: AppBar(
+        title: Text('Create Account'),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              child: online ? Icon(Icons.wifi): Icon(Icons.signal_wifi_off),
             )
-          ],
-          centerTitle: true,
-        ),
-        drawer: buildDrawer(context),
-        body: new Builder(builder: (BuildContext context) {
-          return new Stack(children: _buildAccountForm(context));
-        }));
+          )
+        ],
+        centerTitle: true,
+      ),
+      drawer: buildDrawer(context),
+      body: Builder(builder: (BuildContext context) {
+        return Stack(children: _buildAccountForm(context));
+      })
+    );
   }
 }
